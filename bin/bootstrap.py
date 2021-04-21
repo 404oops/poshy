d09ef619fa79e877161925d0fc6e73f942c229c2
404oops <51266541+404oops@users.noreply.github.com>
2021-04-21 21:22:28 +0200

    Why has this been downgraded again?

--- bin/bootstrap.py
+++ bin/bootstrap.py
@@ -1,7 +1,12 @@
+import bin.ayploader
 import os
-
+import getpass
+un = getpass.getuser()
+id = '$'
+if un == 'root':
+    id = '#'
 while True:
-    app = input("PyOS: $> ")
+    app = input(f"Poshy: {un} {id}> ")
     if app == 'exitos':
         exit()
     try:
