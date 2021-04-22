63402dc465119264fc3ad97c421de9dac165d1bc
Matteo <59450459+matteodev8@users.noreply.github.com>
2021-04-22 18:43:17 +0200

    PoshyWin

--- bin/bootstrap.py
+++ bin/bootstrap.py
@@ -1,16 +1,17 @@
 import bin.ayploader
 import os
 import getpass
+
 un = getpass.getuser()
-id = '$'
-if un == 'root':
-    id = '#'
+
+id = 'PS'
+
 while True:
     app = input(f"Poshy v0.0.7: {un} {id}> ")
     if "cd" in app:
         dir = app[3:]
         os.chdir(dir)
-    if app == 'exitos':
+    if app == 'exit':
         exit()
     try:
         eval(app)
