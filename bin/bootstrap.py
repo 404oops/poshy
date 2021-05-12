25e9e4dc21d2e1b743b264cf1015a1b3529b5b06
Luca <50520164+lucaytpols@users.noreply.github.com>
2021-05-12 10:29:59 +0200

    Fixing some errors.
    Adding except so errors wont show up at all

--- bin/bootstrap.py
+++ bin/bootstrap.py
@@ -1,16 +1,25 @@
 import bin.ayploader
 import os
 import getpass
+import platform
 
 un = getpass.getuser()
 
 id = 'PS'
 
 while True:
-    app = input(f"Poshy v0.0.7: {un} {id}> ")
+    app = input(f"PoshyWin v0.0.7: {un} {id}> ")
     if "cd" in app:
-        dir = app[3:]
-        os.chdir(dir)
+        try:
+            dir = app[3:]
+            os.chdir(dir)
+        except FileNotFoundError:
+            print(f"PoshyWin v0.0.7: {un} {id}> Folder not found!")
+            continue
+        except OSError:
+            print(f"PoshyWin v0.0.7: {un} {id}> Folder not found!")
+            continue
+
     if app == 'exit':
         exit()
     try:
