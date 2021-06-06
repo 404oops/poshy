5d381cf00ab10df8ed1775aeb71bf34666a76dba
404oops <51266541+404oops@users.noreply.github.com>
2021-06-06 17:58:50 +0200

    versioning system implemented + AUTOUPDATES

--- poshy/__init__.py
+++ poshy/__init__.py
@@ -1,7 +1,10 @@
 from . import aya, poshcorn
+import os, shlex, subprocess
 mirror = "404oops.ml/poshcorn"
 VERSION = 0.3
-### VERSION SYSTEM
+### VERSION SYSTEM]
+# BY DEFAULT AUTO-UPDATES ARE ON, PLEASE TURN IT OFF IF YOU DONT WANT AUTO-UPDATES
+au = 1
 import shlex, subprocess
 cmd = "curl -s http://404oops.ml/poshcorn/version"
 d = subprocess.getoutput(cmd)
@@ -9,3 +12,9 @@ d = d[2:]
 d = float(d)
 if d > VERSION:
     print(f"This version of poshy is outdated, please download the newer version of Poshy at Github (Current version: {VERSION}, new: {d})")
+    if au == 1:
+        gp = "git pull"
+        gp = shlex.split(gp)
+        subprocess.run(gp)
+    elif au == 0:
+        print("")
