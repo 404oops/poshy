1b5eda29f59799558cacb4b1baedd73df75e657f
404oops <51266541+404oops@users.noreply.github.com>
2021-06-06 17:47:56 +0200

    If CD is typed in, don't run it through subprocess, and more

--- poshy/__init__.py
+++ poshy/__init__.py
@@ -1,3 +1,11 @@
 from . import aya, poshcorn
-
-VERSION = '0.2'
+mirror = "404oops.ml/poshcorn"
+VERSION = 0.2
+### VERSION SYSTEM
+import shlex, subprocess
+cmd = "curl -s http://404oops.ml/poshcorn/version"
+d = subprocess.getoutput(cmd)
+d = d[2:]
+d = float(d)
+if d > VERSION:
+    print(f"This version of poshy is outdated, please download the newer version of Poshy at Github (Current version: {VERSION}, new: {d})")
