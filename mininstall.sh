311841f4978e1aad2b7f85a44e12e63187e04134
404oops <filip.kujundzic@gmail.com>
2021-09-09 17:02:39 +0200

    major code refractor + exec fix for mac

--- mininstall.sh
+++ mininstall.sh
@@ -1,5 +1,5 @@
 # must be run where 'Start Poshy' is located
 curl https://raw.githubusercontent.com/nic68/minifetch/master/minifetch.py > minifetch.py
-mv minifetch.py poshy/addons/minifetch.py
-echo 'from .addons import minifetch' >> poshy/aya.py
+mv minifetch.py more/addons/minifetch.py
+echo 'from .addons import minifetch' >> more/aya.py
 echo 'Done'
