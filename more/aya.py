8dfdbe1b25c4d022d6e999d3173f27ce6cb3b702
404oops <filip.kujundzic@gmail.com>
2021-09-10 11:34:07 +0200

    major code refractor + exec fix for mac

--- more/aya.py
+++ more/aya.py
@@ -4,4 +4,6 @@ Add Your Addons
 TODO: actually do documentation for this (404oops: it'll be in a gist, don't worry)
 """
 
-from .addons import motd
+# from .addons import motd
+
+# automatically disabled for quick shell startup
