47f88468f95923bbafd2b7e54a43a81edc8ba5d1
404oops <filip.kujundzic@gmail.com>
2021-09-24 15:29:36 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "readme.md").read_text()
 setup(
     name='poshy',
     packages=['aya'],
     version='1.3',
     description='An extensible shell written in python',
-    long_description=long_description,
     long_description_content_type='text/markdown',
     scripts=['bin/poshy']
 )
