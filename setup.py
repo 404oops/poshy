c9188f0cf366d404c017e08333cf75c32253eeff
404oops <filip.kujundzic@gmail.com>
2021-09-24 15:33:15 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='poshy',
     packages=['aya'],
-    version='1.4',
+    version='1.5',
     description='An extensible shell written in python',
     long_description_content_type='text/markdown',
     scripts=['bin/poshy']
