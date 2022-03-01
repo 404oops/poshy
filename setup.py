2c585f0de80b8fa81247152f4eca007e19e6d1f7
404ооps <filip.kujundzic@gmail.com>
2022-03-01 19:45:42 +0100

    Poshy is back, and now it has tab completion for programs in $PATH!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='poshy',
     packages=['aya'],
-    version='1.8',
+    version='2.0',
     description='An extensible shell written in python',
     long_description="""<img title="Poshy" alt="Poshy" src="images/poshy-logo-shadowed.png" width="256">\n\t<h1>Poshy, an extensible shell written in Python</h1>\n</p>\n\nPoshy is a work-in-progress shell written in Python. As of now, it has no configuration files.\n\nTo install, just clone this repo and then run `pip3 install .` and presto!\n\nFor aya developers:\n\nAYA is an extensive python module loader that loads before start. You must have them installed as python modules.\n```python\nfrom setuptools import setup\nsetup(\n    name='youraddonname',\n    packages=['folder'], # the folder in which your __init__.py file is stored in, also must be your addon name\n    version='1.0', # self-explanatory\n    description='the addon description'\n)\n```\nhere's a dummy example of what your setup.py should have. note that to load the addon, you must first install it and then put its name in ~/.aya\n\nyour `__init__.py` is your addon\n\n~/.aya's structure goes as following:\n\nmodules must be line separated (no commas, no same-line whitespace separated things...)\n\nan example would be:\n\n```\nmodule1\nmodule2\nmodule3\n```\nand so on.""",
     long_description_content_type='text/markdown',
